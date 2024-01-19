c925ed47a58b94deb1b628e0cd1b861ba4937618
ssaaresalu <siim.saaresalu@gmail.com>
2024-01-19 16:38:29 +0200

    Initial commit

---
+++ src/main/java/com/example/bakalaureus_api/BakalaureusApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bakalaureus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakalaureusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BakalaureusApiApplication.class, args);
+    }
+
+}
